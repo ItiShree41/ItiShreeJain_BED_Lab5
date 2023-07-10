ad0a0a05bee7359dbe89ce6e2c569a09f790c736
Iti Shree <103059786+ItiShree41@users.noreply.github.com>
2023-07-10 16:37:53 +0530

    Add files via upload

---
+++ ItiShreeJain_BED_Lab5/employee-management/src/main/java/com/greatlearning/EmployeeManagement.java
@@ -0,0 +1,14 @@
+package com.greatlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagement {
+
+	public static void main(String[] args) {
+		// TO DO Auto-generated method stub
+		SpringApplication.run(EmployeeManagement.class, args);
+	}
+
+}
